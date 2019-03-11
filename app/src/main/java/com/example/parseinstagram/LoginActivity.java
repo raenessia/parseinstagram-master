c7dc3f28bd659ebec6ea1a23d2e84154ad64a5fd
raenessia <rray2@bulldogs.aamu.edu>
2019-03-11 12:55:01 -0700

    Initial commit

---
+++ app/src/main/java/com/example/parseinstagram/LoginActivity.java
@@ -0,0 +1,101 @@
+package com.example.parseinstagram;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null){
+            goMainActivity();
+        }
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        //Allows returning users to sign in
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                login(username, password);
+            }
+        });
+
+        //Allows new users to sign up to use the app
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the ParseUser
+                ParseUser user = new ParseUser();
+                // Set core properties
+                user.setUsername(etUsername.getText().toString());
+                user.setPassword(etPassword.getText().toString());
+                user.setEmail( etUsername.getText().toString()+"@example.com");
+                // Invoke signUpInBackground
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            // Hooray! Let them use the app now.
+                            goMainActivity();
+                        } else {
+                            // Sign up didn't succeed. Look at the ParseException
+                            // to figure out what went wrong
+                            Log.e(TAG, "Issue with Sign-up");
+                            e.printStackTrace();
+                            return;
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    // TODO: Better error handling...
+                    Log.e(TAG, "Issue with log-in");
+                    e.printStackTrace();
+                    return;
+                }
+                // TODO : navigate to new activity if the user has signed in properly
+                goMainActivity();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Log.d(TAG, "Navigating to the Main Activity");
+        Intent i= new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/parseinstagram/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.parseinstagram;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.parseinstagram.fragments.ComposeFragment;
+import com.example.parseinstagram.fragments.PostsFragment;
+import com.example.parseinstagram.fragments.ProfileFragment;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new PostsFragment();
+                        //Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_compose:
+                        fragment = new ComposeFragment();
+                        //Toast.makeText(MainActivity.this, "Compose!!!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        fragment = new ProfileFragment();
+                        //Toast.makeText(MainActivity.this, "Profile!!!", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+    }
+}
